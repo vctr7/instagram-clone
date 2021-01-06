5d5328e051da253ce393e0f452077ff97f7532bc
PARK JEE HO <n2201121@gmail.com>
2021-01-06 14:44:11 +0900

    connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
-import { useState } from 'react';
+import {db} from './firebase';
 
 import Header from './component/Header';
 import Post from './component/Post';
@@ -9,76 +10,83 @@ import Direct from './page/Direct';
 import Explore from './page/Explore';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      userImg : "https://www.zotero.org/static/images/icons/edge-old-icon-128%402x.png",
-      username : "vctr",
-      postImg : "https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg",
-      caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
-      comments : [
-        {
-          "user" : "daniel_likees", 
-          "comment" : "가나다라 마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ"
-        },
-        {
-          "user" : "christine", 
-          "comment" : "ㅋㅋㅋㅋㅋ"
-        }
-      ,
-      ],
-      curTime : String(new Date())
-    },
-    {
-      userImg : "https://icon-library.com/images/facebook-icon-32-x-32/facebook-icon-32-x-32-3.jpg",
-      username : "daniel",
-      postImg : "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
-      caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
-      comments : [
-        {
-          "user" : "vvvc", 
-          "comment" : "가나다라 aasdasd"
-        }
-      ],
-      curTime : String(new Date())
-    },
-    {
-      userImg : "https://www.zotero.org/static/images/icons/edge-old-icon-128%402x.png",
-      username : "vctr",
-      postImg : "https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg",
-      caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
-      comments : [
-        {
-          "user" : "daniel_likees", 
-          "comment" : "가나다라 마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ"
-        },
-        {
-          "user" : "christine", 
-          "comment" : "ㅋㅋㅋㅋㅋ"
-        }
-      ,
-      ],
-      curTime : String(new Date())
-    },
-    {
-      userImg : "https://www.zotero.org/static/images/icons/edge-old-icon-128%402x.png",
-      username : "vctr",
-      postImg : "https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg",
-      caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
-      comments : [
-        {
-          "user" : "daniel_likees", 
-          "comment" : "가나다라 마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ"
-        },
-        {
-          "user" : "christine", 
-          "comment" : "ㅋㅋㅋㅋㅋ"
-        }
-      ,
-      ],
-      curTime : String(new Date())
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([
+  //   {
+  //     userImg : "https://www.zotero.org/static/images/icons/edge-old-icon-128%402x.png",
+  //     username : "vctr",
+  //     postImg : "https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg",
+  //     caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
+  //     comments : [
+  //       {
+  //         "user" : "daniel_likees", 
+  //         "comment" : "가나다라 마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ"
+  //       },
+  //       {
+  //         "user" : "christine", 
+  //         "comment" : "ㅋㅋㅋㅋㅋ"
+  //       }
+  //     ,
+  //     ],
+  //     curTime : String(new Date())
+  //   },
+  //   {
+  //     userImg : "https://icon-library.com/images/facebook-icon-32-x-32/facebook-icon-32-x-32-3.jpg",
+  //     username : "daniel",
+  //     postImg : "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+  //     caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
+  //     comments : [
+  //       {
+  //         "user" : "vvvc", 
+  //         "comment" : "가나다라 aasdasd"
+  //       }
+  //     ],
+  //     curTime : String(new Date())
+  //   },
+  //   {
+  //     userImg : "https://www.zotero.org/static/images/icons/edge-old-icon-128%402x.png",
+  //     username : "vctr",
+  //     postImg : "https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg",
+  //     caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
+  //     comments : [
+  //       {
+  //         "user" : "daniel_likees", 
+  //         "comment" : "가나다라 마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ"
+  //       },
+  //       {
+  //         "user" : "christine", 
+  //         "comment" : "ㅋㅋㅋㅋㅋ"
+  //       }
+  //     ,
+  //     ],
+  //     curTime : String(new Date())
+  //   },
+  //   {
+  //     userImg : "https://www.zotero.org/static/images/icons/edge-old-icon-128%402x.png",
+  //     username : "vctr",
+  //     postImg : "https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg",
+  //     caption : "새해 복 많이 받으세요~마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ",
+  //     comments : [
+  //       {
+  //         "user" : "daniel_likees", 
+  //         "comment" : "가나다라 마바사ㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ"
+  //       },
+  //       {
+  //         "user" : "christine", 
+  //         "comment" : "ㅋㅋㅋㅋㅋ"
+  //       }
+  //     ,
+  //     ],
+  //     curTime : String(new Date())
+  //   }
+  // ]);
 
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data()));
+    })
+
+  }, []); 
 
 
   return (
@@ -95,7 +103,7 @@ function App() {
               postImg={post.postImg} 
               caption={post.caption} 
               comments={post.comments} 
-              curTime={post.curTime} 
+              curTime={post.curTime.createdTime} 
             />
           ))}
         </div>
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBain85YZGcfMGwSi8Ylpe0HrZPObZXlHw",
+    authDomain: "instagram-clone-victor.firebaseapp.com",
+    projectId: "instagram-clone-victor",
+    storageBucket: "instagram-clone-victor.appspot.com",
+    messagingSenderId: "655901285872",
+    appId: "1:655901285872:web:2a3e7988592f0bb845d135",
+    measurementId: "G-Z4CEPZRV67"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const storage = firebaseApp.storage();
+
+export { db, auth, storage };
+
+//   export default db;
